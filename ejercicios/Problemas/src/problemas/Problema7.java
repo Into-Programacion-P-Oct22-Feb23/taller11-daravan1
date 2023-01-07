589e6d67667f37f87a3d8de1a97663778442a1c3
Dara <dvan1@utpl.edu.ec>
2023-01-07 11:04:55 -0500

    Segundo commit

--- ejercicios/Problemas/src/problemas/Problema7.java
+++ ejercicios/Problemas/src/problemas/Problema7.java
@@ -5,15 +5,41 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema7 {
+
+    static Scanner entrada = new Scanner(System.in);
+
     public static void main(String[] args) {
-        String cadena = obtenerCiudades();
-        
-        
+        String[] lista = obtenerCiudades();
+        obtenerCaracteres(lista);
     }
-    
+
+    public static String[] obtenerCiudades() {
+        int numero;
+        System.out.println("Ingrese el numero de valores de su lista:\n");
+        numero = entrada.nextInt();
+        entrada.nextLine();
+        String[] cadena = new String [numero];
+        for (int i = 0; i < cadena.length; i++) {
+            System.out.printf("Ingrese el valor de la posicion %d\n", i);
+            cadena[i] = entrada.nextLine();
+        }
+        return cadena;
+    }
+
+    public static void obtenerCaracteres(String[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int caracteres = a[i].length();
+            if (caracteres == 4 || caracteres == 5) {
+                System.out.println(a[i]);
+            }
+
+        }
+    }
 }
